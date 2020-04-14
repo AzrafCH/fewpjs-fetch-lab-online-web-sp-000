e0184f724dc4e9293ae62cddee152d434835eb10
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-14 22:30:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,5 @@ function renderBooks(json) {
 function getPosts() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json)
-    document.querySelector("main").innerHTML = renderBooks(json));
+  .then(json => document.querySelector("main").innerHTML = renderBooks(json))
   }
